34aeaa56ce92448b90d4abc2f156863d1fd2886b
Sharon <captainshar@gmail.com>
2017-05-13 11:48:50 -0700

    nav bar and variables in index.html

--- collection/views.py
+++ collection/views.py
@@ -5,5 +5,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+	#setting variables here
+	number = 6
+	thing = "Eclipse Phase"
 	#index.html for our index view
-	return render(request, 'index.html')
+	return render(request, 'index.html', {
+		#handing variables to the template index.html here
+		'number': number,
+		'thing': thing,
+		})
