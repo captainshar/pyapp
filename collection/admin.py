9955226138c0d2f0795600f7ef0d14f35a27f6df
Sharon <captainshar@gmail.com>
2017-05-13 12:43:53 -0700

    two database entries

--- collection/admin.py
+++ collection/admin.py
@@ -4,3 +4,12 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from collection.models import System
+
+# automated slug creation
+class SystemAdmin(admin.ModelAdmin):
+	model = System
+	list_display = ('name', 'description',)
+	prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(System, SystemAdmin)
